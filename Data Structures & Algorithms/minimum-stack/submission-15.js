cdfe5b42d3a9f316993836bf701e83df9fd0f0bc
donatelloraphael <akbar@xenoscorp.com>
2026-05-02 17:29:04 +0530

    Add: minimum-stack - submission-15

---
+++ Data Structures & Algorithms/minimum-stack/submission-15.js
@@ -0,0 +1,39 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        
+        const currMin = this.minStack.at(-1);
+        this.minStack.push(currMin !== undefined && currMin < val ? currMin: val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack.at(-1);
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack.at(-1);
+    }
+}
