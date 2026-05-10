445131923ae50419a28cab3f9f2dada82e40451c
donatelloraphael <akbar@xenoscorp.com>
2026-05-10 14:01:19 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.ts
@@ -0,0 +1,57 @@
+// G | | | | |
+// 1 1 1
+// 1 2
+// 2 1
+
+
+
+// 1 1 1 1 1
+// 1 1 1 2
+// 1 1 2 1
+// 1 2 1 1
+// 2 1 1 1
+// 2 2 1
+// 2 1 2
+// 1 2 2
+
+
+// 1 1 1 1
+// 1 1 2
+// 1 2 1
+// 2 1 1
+// 2 2
+
+
+
+class Solution {
+    cache: Map<number, number>;
+
+    constructor() {
+        this.cache = new Map<number, number>();
+    }
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n: number): number {
+        if (n === 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
+        const cached = this.cache.get(n);
+
+        if (cached) {
+            return cached;
+        }
+
+        const res = this.climbStairs(n - 1) + this.climbStairs(n - 2);
+
+        this.cache.set(n, res);
+
+        return res;
+    }
+}
