fbe53acc01dc28fe4a444a1188403a1a8a8b34d4
donatelloraphael <akbar@xenoscorp.com>
2026-05-26 18:03:03 +0530

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+    merge(nums1: number[], m: number, nums2: number[], n: number): void {
+        let last = m+n-1;
+        let i = m-1;
+        let j = n-1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[last--] = nums1[i--];
+            } else {
+                nums1[last--] = nums2[j--];
+            }
+        }
+    }
+}
